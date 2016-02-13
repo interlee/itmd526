393ea7d07e08f39235f4d3dd682f89b68228ede9
Daniel Lee <daniellee121@gmail.com>
2016-02-12 22:22:11 -0600

    w04 assignments

--- week04/hw/00-ddl-scripts.sql
+++ week04/hw/00-ddl-scripts.sql
@@ -1,5 +1,6 @@
+------------------
 USE datamart_kbb;
-
+------------------
 DROP TABLE IF EXISTS dim_customer;
 CREATE TABLE `dim_customer` (
   `customer_key` bigint(20) NOT NULL DEFAULT '-1',
@@ -9,10 +10,42 @@ CREATE TABLE `dim_customer` (
   `date_of_birth` datetime DEFAULT NULL,
   `city` varchar(64) DEFAULT '-',
   `state` varchar(64) DEFAULT '-'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
+;
 
+DROP TABLE IF EXISTS dim_customer_scd1;
+CREATE TABLE `dim_customer_scd1` (
+  `customer_key` bigint(20) NOT NULL DEFAULT '-1',
+  `customer_id` varchar(32) NOT NULL DEFAULT '-',
+  `first_name` varchar(32) DEFAULT NULL,
+  `last_name` varchar(32) DEFAULT NULL,
+  `date_of_birth` datetime DEFAULT NULL,
+  `city` varchar(64) DEFAULT '-',
+  `state` varchar(64) DEFAULT '-',
+  `version` bigint(20) DEFAULT NULL,
+  `date_from` datetime DEFAULT NULL,
+  `date_to` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
+;
+
+DROP TABLE IF EXISTS dim_customer_scd2;
+CREATE TABLE `dim_customer_scd2` (
+  `customer_key` bigint(20) NOT NULL DEFAULT '-1',
+  `customer_id` varchar(32) NOT NULL DEFAULT '-',
+  `first_name` varchar(32) DEFAULT NULL,
+  `last_name` varchar(32) DEFAULT NULL,
+  `date_of_birth` datetime DEFAULT NULL,
+  `city` varchar(64) DEFAULT '-',
+  `state` varchar(64) DEFAULT '-',
+  `version` bigint(20) DEFAULT NULL,
+  `date_from` datetime DEFAULT NULL,
+  `date_to` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
+;
+
+--------------
 USE source_db;
-
+--------------
 DROP TABLE IF EXISTS city_state;
 CREATE TABLE `city_state` (
   `city_state_id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -34,8 +67,9 @@ CREATE TABLE `customer` (
   UNIQUE KEY `idx_customer` (`customer_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--------------
 USE target_db;
-
+--------------
 DROP TABLE IF EXISTS stg_customer;
 CREATE TABLE `stg_customer` (
   `customer_id` tinytext,
@@ -44,4 +78,5 @@ CREATE TABLE `stg_customer` (
   `date_of_birth` datetime DEFAULT NULL,
   `city` tinytext,
   `state` tinytext
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
+;
